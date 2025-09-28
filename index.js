fd04e3f9b77cb0eeb515dfd2d78a902d9b075dad
Vanim <vanimouzad123@gmail.com>
2025-09-28 14:14:12 -0300

    feat:vote tales

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const Discord = require('discord.js');
 const cron = require('node-cron');
 const dotenv = require('dotenv')
 dotenv.config()
-const { BOT_TOKEN, CHANNEL_ID } = process.env;
+const { BOT_TOKEN, CHANNEL_ID, ROLE_ID } = process.env;
 
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds ]});
 
 const alertDiscord = () => {
   const channel = client.channels.cache.get(CHANNEL_ID);
 
-  channel.send('Hora de votar / marcar presença de pet! @everyone');
+  channel.send(`Hora de votar no servidor! <@&${ROLE_ID}>`);
 }
 
 client.on('ready', () => {
   console.log('Bot online!');
 
-  cron.schedule('0 21 * * *', alertDiscord);
+  cron.schedule('* * * * *', alertDiscord);
+  // cron.schedule('0 22 * * *', alertDiscord);
 });
 
 client.login(BOT_TOKEN);
